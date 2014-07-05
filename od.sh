13f5e3d51d489e6b2cc859e9a48ea72fab3e3d9c
LEDfan <tobia@ledfan.be>
2014-07-05 12:38:50 +0200

    Use repo instead of master.zip

--- od.sh
+++ od.sh
@@ -24,22 +24,24 @@ echo "[INSTALL] Remove contents in deploy dir"
 rm -rf ./deploy/*
 
 echo "[INSTALL] Downloading ownCloud Core"
-wget https://github.com/owncloud/core/archive/master.zip
-unzip master.zip -d ./deploy
-rm -rf master.zip
-mv ./deploy/core-master/* ./deploy
-rm -rf ./deploy/core-master
+#wget https://github.com/owncloud/core/archive/master.zip
+#unzip master.zip -d ./deploy
+#rm -rf master.zip
+git clone https://github.com/LEDfan/core master
+cd ./master
+git submodule update --init
+rm -rf 3rdparty/.git
+cd ../
+cp -r ./master/.git
+mv ./master/* ./deploy
 
-echo "[INSTALL] Downloading 3rpdarty"
-rm -rf ./deploy/3rdparty
-git clone https://github.com/owncloud/3rdparty ./deploy/3rdparty
-rm -rf ./deploy/3rdparty/.git
+
 
 echo "[INSTALL] Done!"
 
 echo "[APPS] Installing apps: ${apps[@]}"
 
-mkdir -p ./deploy/apps #remove me
+#mkdir -p ./deploy/apps #remove me
 
 cd ./deploy/apps
 for app in "${apps[@]}"
@@ -52,6 +54,7 @@ done
 echo "Deploying on OpenShift"
 echo "====================================="
 cd ../
-git add . -A
+git add --all
 git commit -m "Deploy"
-git push
+git push origin master
+
